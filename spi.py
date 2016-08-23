18155915b86406c0109157043ca9c219357c64c7
Don Coleman <dcoleman@chariotsolutions.com>
2016-08-23 02:26:36 -0400

    python comments (was c)

--- spi.py
+++ spi.py
@@ -1,9 +1,8 @@
-/**
- * Software SPI library based on MRAA.
- *
- * by Seeed Studio (http://seeedstudio.com)
- */
- 
+#
+# Software SPI library based on MRAA.
+#
+# by Seeed Studio (http://seeedstudio.com)
+#
  
 import mraa
 import time
@@ -15,27 +14,27 @@ class SPI():
         self.mosi = mraa.Gpio(mosi)
         self.miso = mraa.Gpio(miso)
         self.cs = mraa.Gpio(cs)
-        
+
         self.sck.dir(mraa.DIR_OUT)
         self.mosi.dir(mraa.DIR_OUT)
         self.miso.dir(mraa.DIR_IN)
         self.cs.dir(mraa.DIR_OUT)
-        
+
         self.cs.write(1)
-        
+
         self.frequency(10000000)
         self.format(8, 0)
-        
+
     def frequency(self, hz=10000000):
         self.freq = hz
-        
+
     def format(self, bits=8, mode=0):
         self.bits = bits
         self.mode = mode
         self.polarity = (mode >> 1) & 1
         self.phase = mode & 1
         self.sck.write(self.polarity)
-        
+
     def write_byte(self, data):
         read = 0
         for bit in range(self.bits - 1, -1, -1):
@@ -52,9 +51,9 @@ class SPI():
 
             self.sck.write(self.polarity)
             time.sleep(0.5 / self.freq)
-        
+
         return read
-        
+
     def write(self, data):
         response = bytearray()
         self.cs.write(0)
@@ -72,10 +71,10 @@ class SPI():
         else:
             self.cs.write(1)
             raise TypeError('%s is not supported' % type(data))
-            
+
         self.cs.write(1)
         return response
-                
+
 if __name__ == '__main__':
     dev = SPI()
     while True:
